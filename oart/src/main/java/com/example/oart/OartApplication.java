b8ac5882306e20df3576c913d3dcd63ccaad3516
hassanbenab <44161562+hassanbenab@users.noreply.github.com>
2022-03-01 11:44:53 +0100

    3 modules added

---
+++ oart/src/main/java/com/example/oart/OartApplication.java
@@ -0,0 +1,13 @@
+package com.example.oart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OartApplication.class, args);
+    }
+
+}
---
+++ oart/src/test/java/com/example/oart/OartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
