beb2ae1c99303060e3767d5126871364fe593bec
Mahdi-MEJRI <mahdi.mejri7817@gmail.com>
2022-03-08 12:00:05 +0100

    page details

--- client/src/main/java/com/example/client/ClientController.java
+++ client/src/main/java/com/example/client/ClientController.java
@@ -3,9 +3,11 @@ package com.example.client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class ClientController {
@@ -16,5 +18,12 @@ public class ClientController {
         List<ProductBean> products = msProductProxy.list();
         model.addAttribute("products", products);
         return "index";
+    }
+
+    @RequestMapping("/product-detail/{id}")
+    public String detail(Model model, @PathVariable Long id) {
+        ProductBean product = msProductProxy.get(id).stream().findFirst().orElse(null);
+        model.addAttribute("product", product);
+        return "detail";
     }
 }
